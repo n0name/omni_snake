48ce97afd6c547dc2a9e187bceb9adc252dedb79
Nikola Georgiev <georgiev.nikito@gmail.com>
2019-04-12 23:30:56 +0300

    Add self collisions and apples

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate piston;
 extern crate graphics;
 extern crate glutin_window;
 extern crate opengl_graphics;
+extern crate rand;
 
 use math2d::math::common::*;
 use math2d::math::vector::Vec2D;
@@ -15,9 +16,13 @@ use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{ GlGraphics, OpenGL };
 use std::f64::consts::PI;
 use std::collections::VecDeque;
+use rand::Rng;
 
-const SEGMENT_RADIUS: f64 = 5.0;    // [Pix]
-const UPDATE_TIME: f64 = 0.1;       // [S]
+const SEGMENT_RADIUS: f64 = 5.0;        // [Pix]
+const APPLE_RADIUS: f64 = 5.0;          // [Pix]
+const UPDATE_TIME: f64 = 0.1;           // [S]
+const SPAWN_TIME: f64 = 5.0;            // [S]
+const SEGMENTS_PER_APPLE: usize = 3;    // [#]
 
 struct Snake {
     dir: Vec2D,
@@ -41,7 +46,16 @@ impl Snake {
 pub struct App {
     gl: GlGraphics, // OpenGL drawing backend.
     snake: Snake,
-    last_move: f64
+    apples: Vec<Vec2D>,
+    last_move: f64,
+    last_spawn: f64,
+    new_segments: usize
+}
+
+enum CollisionType {
+    No,
+    Apple,
+    Snake
 }
 
 impl App {
@@ -53,10 +67,19 @@ impl App {
         const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
 
         let segment_graphics = ellipse::Ellipse::new(GREEN);
+        let apple_graphics = ellipse::Ellipse::new(RED);
         let snake = &self.snake;
+        let apples = &self.apples;
 
         self.gl.draw(args.viewport(), |c, gl| {
             clear(BLACK, gl);
+
+            apples.iter().for_each(|a| {
+                let (x, y) = a.as_tuple();
+                let transform = c.transform.trans(x - 250.0, y - 250.0);
+                apple_graphics.draw(ellipse::circle(x, y, APPLE_RADIUS), &c.draw_state, transform, gl);
+            });
+            
             snake.segments.iter().for_each(|s|{
                 let (x, y) = s.as_tuple();
                 let transform = c.transform.trans(x - 250.0, y - 250.0);
@@ -65,16 +88,97 @@ impl App {
         });
     }
 
-    fn update(&mut self, args: &UpdateArgs) {
+    fn check_collisions(&mut self, head: &Vec2D) -> CollisionType {
+        let mut found_snake = false;
+
+        for s in self.snake.segments.iter() {
+            if s.distance(head) <= (SEGMENT_RADIUS / 2.0) {
+                found_snake = true;
+                break;
+            }
+        }
+
+        if found_snake {
+            return CollisionType::Snake;
+        }
+
+        let mut found_apple = false;
+        let mut to_remove = Vec::new();
+        self.apples.iter().enumerate().for_each(|(i, a)| {
+            if a.distance(head) <= (SEGMENT_RADIUS + APPLE_RADIUS) {
+                found_apple = true;
+                to_remove.push(i);
+            }
+        });
+
+        to_remove.iter().for_each(|i| {
+            self.apples.remove(*i);
+        });
+        if found_apple {
+            return CollisionType::Apple;
+        } else {
+            return CollisionType::No;
+        }
+    }
+
+    fn calc_next_possition(&self) -> Vec2D {
+        let dir = self.snake.dir;
+        let head = self.snake.segments.front().unwrap();
+        let mut seg = *head + self.snake.dir * SEGMENT_RADIUS;
+
+        const LOWER_BOUND : f64 = 110.0;
+        const UPPER_BOUND : f64 = 390.0;
+
+        if seg.x - SEGMENT_RADIUS < LOWER_BOUND && dir.x < 0.0 {
+            seg.x = UPPER_BOUND - SEGMENT_RADIUS;
+        }
+        if seg.x + SEGMENT_RADIUS >= UPPER_BOUND && dir.x > 0.0 {
+            seg.x = LOWER_BOUND + SEGMENT_RADIUS;
+        }
+
+        if seg.y - SEGMENT_RADIUS < LOWER_BOUND && dir.y < 0.0 {
+            seg.y = UPPER_BOUND - SEGMENT_RADIUS;
+        }
+
+        if seg.y + SEGMENT_RADIUS >= UPPER_BOUND && dir.y > 0.0 {
+            seg.y = LOWER_BOUND + SEGMENT_RADIUS;
+        }
+
+        // println!("New Pos: {:?}", seg);
+
+        return seg;
+    }
+
+    fn update(&mut self, args: &UpdateArgs) -> bool {
         self.last_move -= args.dt;
         if self.last_move <= 0.0 {
             self.last_move = UPDATE_TIME;
-            let head = self.snake.segments.front().unwrap() ;
-            let seg = *head + self.snake.dir * SEGMENT_RADIUS;
-            self.snake.segments.pop_back();
+            let seg = self.calc_next_possition();
+            let collision_type = self.check_collisions(&seg);
+            match collision_type {
+                CollisionType::Apple => self.new_segments += SEGMENTS_PER_APPLE,
+                CollisionType::Snake => return true,
+                _ => ()
+            }
+            if self.new_segments == 0 {
+                self.snake.segments.pop_back();
+            } else {
+                self.new_segments -= 1;
+            }
             self.snake.segments.push_front(seg);
         }
+
+        self.last_spawn -= args.dt;
+        if self.last_spawn <= 0.0 {
+            self.last_spawn = SPAWN_TIME;
+            let mut rng = rand::thread_rng();
+            let x = rng.gen_range(100.0, 400.0);
+            let y = rng.gen_range(100.0, 400.0);
+            self.apples.push(Vec2D::new(x, y));
+        }
+        return false;
     }
+    
 }
 
 fn main() {
@@ -94,7 +198,10 @@ fn main() {
     let mut app = App {
         gl: GlGraphics::new(opengl),
         snake: Snake::new(Vec2D::new(250.0, 250.0), Vec2D::new(-1.0, 0.0)),
-        last_move: 0.0
+        apples: Vec::new(),
+        last_move: 0.0,
+        last_spawn: 0.0,
+        new_segments: 0
     };
 
     let mut events = Events::new(EventSettings::new());
@@ -112,7 +219,9 @@ fn main() {
         }
 
         if let Some(u) = e.update_args() {
-            app.update(&u);
+            if app.update(&u) {
+                break;
+            }
         }
     }
 }
